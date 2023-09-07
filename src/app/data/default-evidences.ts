932ad46f2946d8a8836480fc12d1283e7c0f3e75
robbsnor <robbinschmidt@gmail.com>
2023-09-07 19:05:28 +0200

    feat: moved data

---
+++ src/app/data/default-evidences.ts
@@ -0,0 +1,32 @@
+import { Evidence } from '../models/evidence.model';
+
+export const DEFAULT_EVIDENCES: Evidence[] = [
+  {
+    name: 'EMF Level 5',
+    status: 'faded',
+  },
+  {
+    name: 'D.O.T.S. Projector',
+    status: 'faded',
+  },
+  {
+    name: 'Ultra Violet',
+    status: 'faded',
+  },
+  {
+    name: 'Ghost Orb',
+    status: 'faded',
+  },
+  {
+    name: 'Ghost Writing',
+    status: 'faded',
+  },
+  {
+    name: 'Spirit Box',
+    status: 'faded',
+  },
+  {
+    name: 'Freezing Temperatures',
+    status: 'faded',
+  },
+];
---
+++ src/app/data/default-ghosts.ts
@@ -0,0 +1,124 @@
+import { Ghost } from '../models/ghost.model';
+
+export const DEFAULT_GHOSTS: Ghost[] = [
+  {
+    name: 'Spirit',
+    evidences: ['EMF Level 5', 'Spirit Box', 'Ultra Violet'],
+    status: 'shown',
+  },
+  {
+    name: 'Wraith',
+    evidences: ['Freezing Temperatures', 'Spirit Box', 'Ultra Violet'],
+    status: 'shown',
+  },
+  {
+    name: 'Phantom',
+    evidences: ['EMF Level 5', 'Ghost Orb', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Poltergeist',
+    evidences: ['Spirit Box', 'Ultra Violet', 'Ghost Orb'],
+    status: 'shown',
+  },
+  {
+    name: 'Banshee',
+    evidences: ['EMF Level 5', 'Ultra Violet', 'Ghost Orb'],
+    status: 'shown',
+  },
+  {
+    name: 'Jinn',
+    evidences: ['EMF Level 5', 'Spirit Box', 'Ghost Orb'],
+    status: 'shown',
+  },
+  {
+    name: 'Mare',
+    evidences: ['Spirit Box', 'Ghost Orb', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Revenant',
+    evidences: ['EMF Level 5', 'Ultra Violet', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Shade',
+    evidences: ['EMF Level 5', 'Ghost Orb', 'Ghost Writing'],
+    status: 'shown',
+  },
+  {
+    name: 'Demon',
+    evidences: ['Spirit Box', 'Ghost Writing', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Yurei',
+    evidences: ['Ghost Orb', 'Ghost Writing', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Oni',
+    evidences: ['EMF Level 5', 'Spirit Box', 'Ghost Writing'],
+    status: 'shown',
+  },
+  {
+    name: 'Yokai',
+    evidences: ['Spirit Box', 'Ghost Orb', 'D.O.T.S. Projector'],
+    status: 'shown',
+  },
+  {
+    name: 'Hantu',
+    evidences: ['Ultra Violet', 'Ghost Orb', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Goryo',
+    evidences: ['EMF Level 5', 'Ultra Violet', 'D.O.T.S. Projector'],
+    status: 'shown',
+  },
+  {
+    name: 'Myling',
+    evidences: ['EMF Level 5', 'Ultra Violet', 'Ghost Writing'],
+    status: 'shown',
+  },
+  {
+    name: 'Onryo',
+    evidences: ['Spirit Box', 'Ghost Orb', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'The Twins',
+    evidences: ['EMF Level 5', 'Spirit Box', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Raiju',
+    evidences: ['EMF Level 5', 'Ghost Orb', 'D.O.T.S. Projector'],
+    status: 'shown',
+  },
+  {
+    name: 'Obake',
+    evidences: ['EMF Level 5', 'Ultra Violet', 'Ghost Orb'],
+    status: 'shown',
+  },
+  {
+    name: 'The Mimic',
+    evidences: ['Spirit Box', 'Ultra Violet', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Moroi',
+    evidences: ['Spirit Box', 'Ghost Writing', 'Freezing Temperatures'],
+    status: 'shown',
+  },
+  {
+    name: 'Deogen',
+    evidences: ['Spirit Box', 'Ghost Writing', 'D.O.T.S. Projector'],
+    status: 'shown',
+  },
+  {
+    name: 'Thaye',
+    evidences: ['Ghost Orb', 'Ghost Writing', 'D.O.T.S. Projector'],
+    status: 'shown',
+  },
+];
