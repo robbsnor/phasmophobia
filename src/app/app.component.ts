577aaf2f606694024b3adc586af86df618049c45
robbsnor <robbinschmidt@gmail.com>
2023-09-07 21:59:35 +0200

    reset button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,31 +10,14 @@ import { DEFAULT_EVIDENCES } from './data/default-evidences';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public evidences = DEFAULT_EVIDENCES;
-  public ghosts = DEFAULT_GHOSTS;
+  public evidences = structuredClone(DEFAULT_EVIDENCES);
+  public ghosts = structuredClone(DEFAULT_GHOSTS);
 
-  public changeEvidenceStatus(evidence: Evidence) {
-    const evidenceToChange = this.evidences.find(
-      (posibleEvidence) => evidence.name === posibleEvidence.name
-    );
-    if (evidenceToChange) {
-      if (evidenceToChange.status === 'shown') {
-        evidenceToChange.status = 'faded';
-      } else if (evidenceToChange.status === 'faded') {
-        evidenceToChange.status = 'shown';
-      }
-    }
-
-    this.updatePosibleGhosts();
-  }
-
-  public updatePosibleGhosts() {
+  private updatePosibleGhosts() {
     const selectedEvidenceNames = this.evidences
       .filter((evidence) => evidence.status === 'shown')
       .map((evidence) => evidence.name);
 
-    // spirit:    ['EMF Level 5', 'Spirit Box', 'Ultra Violet']
-    // shade:     ['EMF Level 5', 'Ghost Orb', 'Ghost Writing']
     const posibleGhostNames = DEFAULT_GHOSTS.filter((ghost) =>
       selectedEvidenceNames.every((evidenceName) =>
         ghost.evidences.includes(evidenceName)
@@ -50,5 +33,25 @@ export class AppComponent {
       ghost.status = 'faded';
       return ghost;
     });
+  }
+
+  public reset() {
+    this.evidences = structuredClone(DEFAULT_EVIDENCES);
+    this.ghosts = structuredClone(DEFAULT_GHOSTS);
+  }
+
+  public changeEvidenceStatus(evidence: Evidence) {
+    const evidenceToChange = this.evidences.find(
+      (posibleEvidence) => evidence.name === posibleEvidence.name
+    );
+    if (evidenceToChange) {
+      if (evidenceToChange.status === 'shown') {
+        evidenceToChange.status = 'faded';
+      } else if (evidenceToChange.status === 'faded') {
+        evidenceToChange.status = 'shown';
+      }
+    }
+
+    this.updatePosibleGhosts();
   }
 }
